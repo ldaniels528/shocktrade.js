d343ba8f32ec188abfad3216338c4f16b2ad65eb
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-07-02 10:16:33 -0700

    added function to return a dynamic instance of the scope

--- app-js/src/main/scala/com/ldaniels528/javascript/angularjs/Scope.scala
+++ app-js/src/main/scala/com/ldaniels528/javascript/angularjs/Scope.scala
@@ -1,7 +1,13 @@
 package com.ldaniels528.javascript.angularjs
 
+import scala.scalajs.js
+
 /**
  * Marker Trait for an Angular.js Scope
  * @author lawrence.daniels@gmail.com
  */
-trait Scope extends biz.enef.angulate.Scope
+trait Scope extends biz.enef.angulate.Scope {
+
+  def dynamic = this.asInstanceOf[js.Dynamic]
+
+}
