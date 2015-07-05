85ae67f31f6bd7a39f60264dff1c924089d52a54
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-07-04 18:31:58 -0700

    Fixed issues with avatar directive

--- app-js/src/main/scala/com/shocktrade/javascript/directives/AvatarDirective.scala
+++ app-js/src/main/scala/com/shocktrade/javascript/directives/AvatarDirective.scala
@@ -5,8 +5,7 @@ import com.ldaniels528.scalascript.core.{Attributes, JQLite}
 import com.ldaniels528.scalascript.{Directive, Scope}
 
 import scala.scalajs.js
-import scala.scalajs.js.Dynamic.{global => g, literal => JS}
-import scala.scalajs.js.JSON
+import scala.scalajs.js.Dynamic.{literal => JS}
 
 /**
  * Avatar Directive
@@ -15,38 +14,25 @@ import scala.scalajs.js.JSON
  */
 class AvatarDirective extends Directive[AvatarDirectiveScope] {
   override val restrict = "E"
-  override val scope = JS(id = "@id", `object` = "@object", alt = "@alt", `class` = "@class", style = "@style")
+  override val scope = JS(id = "@id", alt = "@alt", `class` = "@class", style = "@style")
   override val transclude = true
   override val replace = false
   override val template = """<img ng-src="{{ url }}" ng-class="myClass" class="{{ class }}" style="{{ style }}">"""
 
-  def link(scope: AvatarDirectiveScope, element: JQLite, attrs: Attributes) = {
-    scope.$watch("object", (newValue: js.Any, oldValue: js.Any) => populateScope(scope, newValue, oldValue))
+  override def link(scope: AvatarDirectiveScope, element: JQLite, attrs: Attributes) = {
     scope.$watch("id", (newValue: js.Any, oldValue: js.Any) => populateScope(scope, newValue, oldValue))
   }
 
   private def populateScope(scope: AvatarDirectiveScope, newValue: js.Any, oldValue: js.Any) {
-    if (isDefined(scope.id)) {
+    if (scope.id.nonBlank) {
       scope.url = s"http://graph.facebook.com/${scope.id}/picture"
       scope.myClass = "playerAvatar"
     }
-    else if (isDefined(scope.`object`)) {
-      scope.myClass = "playerAvatar"
-      val json = JSON.parse(scope.`object`)
-      if (!isDefined(json)) scope.url = if (isDefined(scope.alt)) scope.alt else "/assets/images/avatars/avatar100.png"
-      else if (isDefined(json.picture)) scope.url = json.picture.data.url.as[String]
-      else if (isDefined(json.facebookID)) scope.url = s"http://graph.facebook.com/${json.facebookID}/picture"
-      else {
-        g.console.error(s"Avatar object type could not be determined - ${scope.`object`}")
-        scope.url = if (isDefined(scope.alt)) scope.alt else "/assets/images/avatars/avatar100.png"
-      }
-    }
     else {
-      scope.url = if (isDefined(scope.alt)) scope.alt else "/assets/images/avatars/avatar100.png"
+      scope.url = if (scope.alt.nonBlank) scope.alt else "/assets/images/avatars/avatar100.png"
       scope.myClass = "spectatorAvatar"
     }
   }
-
 }
 
 /**
@@ -55,7 +41,6 @@ class AvatarDirective extends Directive[AvatarDirectiveScope] {
  */
 trait AvatarDirectiveScope extends Scope {
   var id: String = js.native
-  var `object`: String = js.native
   var alt: String = js.native
   var `class`: String = js.native
   var style: String = js.native
