bf2f89392a2a0340ae2a24c6abcc63622074f0c8
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-07-22 09:31:05 -0700

    converted expanders to Play templates

--- app-play/app/com/shocktrade/controllers/Application.scala
+++ app-play/app/com/shocktrade/controllers/Application.scala
@@ -16,42 +16,78 @@ object Application extends Controller with MongoController {
 
   def index = Action(Ok(assets.views.html.index(UUID.randomUUID().toString)))
 
+  def inspectView = Action(Ok(assets.views.html.admin.Inspect()))
+
+  def webSocket = WebSocket.acceptWithActor[JsValue, JsValue] { request => out => WebSocketHandler.props(out) }
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //    About Views
+  ///////////////////////////////////////////////////////////////////////////////
+
   def aboutMe = Action(Ok(assets.views.html.about.AboutMe()))
 
   def aboutUs = Action(Ok(assets.views.html.about.AboutUs()))
 
-  def chatView() = Action(Ok(assets.views.html.dashboard.Chat()))
-
-  def connectView() = Action(Ok(assets.views.html.connect.Connect()))
-
-  def dashboardView() = Action(Ok(assets.views.html.dashboard.Dashboard()))
-
-  def discoverView() = Action(Ok(assets.views.html.discover.Discover()))
-
-  def exploreView() = Action(Ok(assets.views.html.discover.Explore()))
-
-  def exposureView() = Action(Ok(assets.views.html.dashboard.Exposure()))
-
-  def homeView() = Action(Ok(assets.views.html.profile.Home()))
-
-  def inspectView() = Action(Ok(assets.views.html.admin.Inspect()))
-
   def investors() = Action(Ok(assets.views.html.about.Investors()))
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //    Contest Views
+  ///////////////////////////////////////////////////////////////////////////////
+
+  def chatView = Action(Ok(assets.views.html.dashboard.Chat()))
+
+  def dashboardView = Action(Ok(assets.views.html.dashboard.Dashboard()))
+
+  def exposureView = Action(Ok(assets.views.html.dashboard.Exposure()))
+
+  def ordersActiveView = Action(Ok(assets.views.html.dashboard.ActiveOrders()))
+
+  def ordersClosedView = Action(Ok(assets.views.html.dashboard.ClosedOrders()))
+
+  def performanceView = Action(Ok(assets.views.html.dashboard.Performance()))
+
+  def positionsView = Action(Ok(assets.views.html.dashboard.Positions()))
+
+  def searchView = Action(Ok(assets.views.html.search.Search()))
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //    Discover Views
+  ///////////////////////////////////////////////////////////////////////////////
+
+  def discoverView = Action(Ok(assets.views.html.discover.Discover()))
+
+  def balanceSheetView = Action(Ok(assets.views.html.discover.expanders.BalanceSheet()))
+
+  def dividendsSplitsView = Action(Ok(assets.views.html.discover.expanders.DividendsSplits()))
+
+  def incomeStatementView = Action(Ok(assets.views.html.discover.expanders.IncomeStatement()))
 
   def newsCenterView = Action(Ok(assets.views.html.news.NewsCenter()))
 
-  def ordersActiveView() = Action(Ok(assets.views.html.dashboard.ActiveOrders()))
+  def pricePerformanceView = Action(Ok(assets.views.html.discover.expanders.PricePerformance()))
 
-  def ordersClosedView() = Action(Ok(assets.views.html.dashboard.ClosedOrders()))
+  def shareStatisticsView = Action(Ok(assets.views.html.discover.expanders.ShareStatistics()))
 
-  def performanceView() = Action(Ok(assets.views.html.dashboard.Performance()))
+  def valuationMeasuresView = Action(Ok(assets.views.html.discover.expanders.ValuationMeasures()))
 
-  def positionsView() = Action(Ok(assets.views.html.dashboard.Positions()))
+  ///////////////////////////////////////////////////////////////////////////////
+  //    Explore Views
+  ///////////////////////////////////////////////////////////////////////////////
 
-  def researchView() = Action(Ok(assets.views.html.research.Research()))
+  def exploreView = Action(Ok(assets.views.html.discover.Explore()))
 
-  def searchView() = Action(Ok(assets.views.html.search.Search()))
+  ///////////////////////////////////////////////////////////////////////////////
+  //    Research Views
+  ///////////////////////////////////////////////////////////////////////////////
 
-  def webSocket = WebSocket.acceptWithActor[JsValue, JsValue] { request => out => WebSocketHandler.props(out) }
+  def researchView = Action(Ok(assets.views.html.research.Research()))
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //    Home Views
+  ///////////////////////////////////////////////////////////////////////////////
+
+  def connectView = Action(Ok(assets.views.html.connect.Connect()))
+
+  def homeView = Action(Ok(assets.views.html.profile.Home()))
 
 }
