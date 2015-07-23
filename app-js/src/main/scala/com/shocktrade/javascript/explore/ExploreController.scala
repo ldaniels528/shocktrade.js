25bb2da164dc1904a424657c4ceb3948aecc2b9e
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-07-23 12:19:02 -0700

    clean-up and consolidation

--- app-js/src/main/scala/com/shocktrade/javascript/explore/ExploreController.scala
+++ app-js/src/main/scala/com/shocktrade/javascript/explore/ExploreController.scala
@@ -1,11 +1,12 @@
 package com.shocktrade.javascript.explore
 
+import com.github.ldaniels528.scalascript.ScalaScriptOps._
 import com.github.ldaniels528.scalascript.core.{Location, Timeout}
 import com.github.ldaniels528.scalascript.extensions.{AnchorScroll, Cookies, Toaster}
 import com.github.ldaniels528.scalascript.{Controller, Scope, injected, scoped}
 import com.shocktrade.javascript.ScalaJsHelper._
 import com.shocktrade.javascript.explore.ExploreController._
-import com.shocktrade.javascript.explore.ExploreService.{SectorInfo, SectorQuote}
+import com.shocktrade.javascript.explore.ExploreService.{AggregatedData, SectorInfo, SectorQuote}
 import org.scalajs.dom.console
 
 import scala.concurrent.Future
@@ -37,24 +38,17 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
   def expandOrCollapseSector(aSector: js.UndefOr[Sector]) = aSector foreach toggleSector
 
   @scoped
-  def expandOrCollapseIndustry(aSector: js.UndefOr[Sector], aIndustry: js.UndefOr[Industry]) = {
-    for {
-      sector <- aSector.toOption
-      industry <- aIndustry.toOption
-    } {
-      toggleIndustry(sector, industry)
-    }
+  def expandOrCollapseIndustry(aSector: js.UndefOr[Sector], aIndustry: js.UndefOr[Industry]) {
+    for {sector <- aSector.toOption; industry <- aIndustry.toOption} toggleIndustry(sector, industry)
   }
 
   @scoped
-  def expandOrCollapseSubIndustry(aSector: js.UndefOr[Sector], aIndustry: js.UndefOr[Industry], aSubIndustry: js.UndefOr[SubIndustry]) = {
+  def expandOrCollapseSubIndustry(aSector: js.UndefOr[Sector], aIndustry: js.UndefOr[Industry], aSubIndustry: js.UndefOr[SubIndustry]) {
     for {
       sector <- aSector.toOption
       industry <- aIndustry.toOption
       subIndustry <- aSubIndustry.toOption
-    } {
-      toggleSubIndustry(sector, industry, subIndustry)
-    }
+    } toggleSubIndustry(sector, industry, subIndustry)
   }
 
   @scoped
@@ -63,8 +57,6 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
       case Success(data) =>
         $scope.sectors = data.map { v => Sector(label = v._id, total = v.total) }
         console.log(s"Loaded ${data.length} sectors")
-
-        // expand the sector, industry, sub-industry for the current symbol
         $timeout(() => expandSymbolsSector($scope.selectedSymbol), 500)
       case Failure(e) =>
         toaster.error("Failed to refresh sector information")
@@ -75,7 +67,7 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
   //          Private Functions
   /////////////////////////////////////////////////////////////////////
 
-  private def expandSymbolsSector(symbol: String) = {
+  private def expandSymbolsSector(symbol: String) {
     console.log(s"Attempting to expand sectors for symbol $symbol...")
     for {
       info <- exploreService.loadSectorInfo(symbol)
@@ -86,10 +78,10 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
       _ = console.log(s"Expanding industry ${info.industry}...")
       industry <- expandIndustry(info, sector.get) if sector.isDefined
 
-      _ = console.log(s"Expanding subIndustry ${info.subIndustry}...")
+      _ = console.log(s"Expanding sub-industry ${info.subIndustry}...")
       subIndustry <- expandSubIndustry(info, sector.get, industry.get) if industry.isDefined
     } {
-      //$location.hash(symbol)
+      $location.hash(symbol)
       $anchorScroll(symbol)
     }
   }
@@ -98,18 +90,12 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
     val result = for {
       sectorName <- info.sector.toOption
       sector <- $scope.sectors.find(_.label == sectorName)
-      expanded = sector.expanded.exists(_ == true)
-    } yield (sector, expanded)
+    } yield (sector, sector.expanded.contains(true))
 
     result match {
       case Some((sector, expanded)) if !expanded =>
         sector.loading = true
-        exploreService.loadIndustries(sector.label) map { data =>
-          sector.loading = false
-          sector.industries = data.map { v => Industry(label = v._id, total = v.total) }
-          sector.expanded = true
-          Some(sector)
-        }
+        exploreService.loadIndustries(sector.label) map (updateSector(sector, _))
       case _ => Future.successful(None)
     }
   }
@@ -118,18 +104,12 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
     val result = for {
       industryName <- info.industry.toOption
       industry <- sector.industries.find(_.label == industryName)
-      expanded = industry.expanded.exists(_ == true)
-    } yield (industry, expanded)
+    } yield (industry, industry.expanded.contains(true))
 
     result match {
       case Some((industry, expanded)) if !expanded =>
         industry.loading = true
-        exploreService.loadSubIndustries(sector.label, industry.label) map { data =>
-          industry.loading = false
-          industry.subIndustries = data.map { v => SubIndustry(label = v._id, total = v.total) }
-          industry.expanded = true
-          Some(industry)
-        }
+        exploreService.loadSubIndustries(sector.label, industry.label) map(updateIndustry(industry, _))
       case _ => Future.successful(None)
     }
   }
@@ -138,30 +118,21 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
     val result = for {
       subIndustryName <- info.subIndustry.toOption
       subIndustry <- industry.subIndustries.find(_.label == subIndustryName)
-      expanded = subIndustry.expanded.exists(_ == true)
-    } yield (subIndustry, expanded)
+    } yield (subIndustry, subIndustry.expanded.contains(true))
 
     result match {
       case Some((subIndustry, expanded)) if !expanded =>
         subIndustry.loading = true
-        exploreService.loadIndustryQuotes(sector.label, industry.label, subIndustry.label) map { quotes =>
-          subIndustry.loading = false
-          subIndustry.quotes = quotes
-          subIndustry.expanded = true
-          Some(subIndustry)
-        }
+        exploreService.loadIndustryQuotes(sector.label, industry.label, subIndustry.label) map(updateSubIndustry(subIndustry, _))
       case _ => Future.successful(None)
     }
   }
 
   private def toggleSector(sector: Sector) {
-    if (!sector.expanded.exists(_ == true)) {
+    if (!sector.expanded.contains(true)) {
       sector.loading = true
       exploreService.loadIndustries(sector.label) onComplete {
-        case Success(data) =>
-          sector.loading = false
-          sector.industries = data.map { v => Industry(label = v._id, total = v.total) }
-          sector.expanded = true
+        case Success(data) => updateSector(sector, data)
         case Failure(e) => sector.loading = false
       }
     }
@@ -169,13 +140,10 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
   }
 
   private def toggleIndustry(sector: Sector, industry: Industry) {
-    if (!industry.expanded.exists(_ == true)) {
+    if (!industry.expanded.contains(true)) {
       industry.loading = true
       exploreService.loadSubIndustries(sector.label, industry.label) onComplete {
-        case Success(data) =>
-          industry.loading = false
-          industry.subIndustries = data.map { v => SubIndustry(label = v._id, total = v.total) }
-          industry.expanded = true
+        case Success(data) => updateIndustry(industry, data)
         case Failure(e) => industry.loading = false
       }
     }
@@ -183,22 +151,37 @@ class ExploreController($scope: ExploreScope, $anchorScroll: AnchorScroll, $cook
   }
 
   private def toggleSubIndustry(sector: Sector, industry: Industry, subIndustry: SubIndustry) {
-    if (!subIndustry.expanded.exists(_ == true)) {
+    if (!subIndustry.expanded.contains(true)) {
       subIndustry.loading = true
-      val mySubIndustry = if (isDefined(subIndustry)) subIndustry.label else null
-      exploreService.loadIndustryQuotes(sector.label, industry.label, mySubIndustry) onComplete {
-        case Success(quotes) =>
-          subIndustry.loading = false
-          subIndustry.quotes = quotes
-          subIndustry.expanded = true
+      exploreService.loadIndustryQuotes(sector.label, industry.label, subIndustry.label) onComplete {
+        case Success(quotes) => updateSubIndustry(subIndustry, quotes)
         case Failure(e) => subIndustry.loading = false
       }
     }
     else subIndustry.expanded = false
   }
 
-}
+  private def updateSector(sector: Sector, data: js.Array[AggregatedData]) = {
+    sector.loading = false
+    sector.industries = data.map { v => Industry(label = v._id, total = v.total) }
+    sector.expanded = true
+    Some(sector)
+  }
 
+  private def updateIndustry(industry: Industry, data: js.Array[AggregatedData]) = {
+    industry.loading = false
+    industry.subIndustries = data.map { v => SubIndustry(label = v._id, total = v.total) }
+    industry.expanded = true
+    Some(industry)
+  }
+
+  private def updateSubIndustry(subIndustry: SubIndustry, quotes: js.Array[SectorQuote]) = {
+    subIndustry.loading = false
+    subIndustry.quotes = quotes
+    subIndustry.expanded = true
+    Some(subIndustry)
+  }
+}
 
 /**
  * Explore Controller
@@ -230,7 +213,7 @@ object ExploreController {
     var label: String = js.native
     var total: Int = js.native
     var expanded: js.UndefOr[Boolean] = js.native
-    var loading: Boolean = js.native
+    var loading: js.UndefOr[Boolean] = js.native
   }
 
   /**
@@ -289,5 +272,4 @@ object ExploreController {
       subIndustry
     }
   }
-
 }
