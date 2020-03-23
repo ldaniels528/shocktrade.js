912a2f281dfd0056489c26956d394d04ea7a8035
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2020-03-22 18:01:42 -0700

    dashboard user interface tweaking

--- app/frontends/angularjs/src/main/scala/com/shocktrade/client/contest/ExposureController.scala
+++ app/frontends/angularjs/src/main/scala/com/shocktrade/client/contest/ExposureController.scala
@@ -3,9 +3,11 @@ package com.shocktrade.client.contest
 import com.shocktrade.client.RootScope
 import com.shocktrade.client.ScopeEvents._
 import com.shocktrade.client.contest.DashboardController.DashboardRouteParams
-import com.shocktrade.client.contest.ExposureController.ExposureSelection
+import com.shocktrade.client.contest.ExposureController._
+import com.shocktrade.client.models.UserProfile
 import com.shocktrade.client.users.GameStateFactory
 import com.shocktrade.common.models.ExposureData
+import io.scalajs.dom.Event
 import io.scalajs.dom.html.browser.console
 import io.scalajs.npm.amcharts.AmChart.Export
 import io.scalajs.npm.amcharts.{AmCharts, AmPieChart}
@@ -36,12 +38,11 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
   //          Public Variables
   ///////////////////////////////////////////////////////////////////////////
 
-  $scope.data = null
   $scope.exposures = js.Array(
-    new ExposureSelection(value = "sector", label = "Sector Exposure"),
-    new ExposureSelection(value = "industry", label = "Industry Exposure"),
+    new ExposureSelection(value = "securities", label = "Securities Exposure"),
     new ExposureSelection(value = "exchange", label = "Exchange Exposure"),
-    new ExposureSelection(value = "securities", label = "Securities Exposure"))
+    new ExposureSelection(value = "industry", label = "Industry Exposure"),
+    new ExposureSelection(value = "sector", label = "Sector Exposure"))
 
   $scope.selectedExposure = $scope.exposures.headOption.orUndefined
   $scope.options = new ChartOptions(
@@ -61,31 +62,28 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
   //          Initialization Functions
   ///////////////////////////////////////////////////////////////////////////
 
-  def init(): Unit = {
+  $scope.initChart = () => initChart()
+
+  def initChart(): Unit = {
     console.info(s"Initializing ${getClass.getSimpleName}...")
-    for {
-      contestID <- $routeParams.contestID
-      userID <- gameState.userID
-    } $scope.exposureChart(contestID, userID, $scope.exposures.headOption.orUndefined)
+    $scope.exposureChart($scope.selectedExposure)
   }
 
-  $scope.onUserProfileUpdated { (_, _) => init() }
+  $scope.onUserProfileUpdated { (_: Event, _: UserProfile) => initChart() }
 
   ///////////////////////////////////////////////////////////////////////////
   //          Public Functions
   ///////////////////////////////////////////////////////////////////////////
 
-  $scope.exposureChart = (aContestID: js.UndefOr[String], aUserID: js.UndefOr[String], anExposure: js.UndefOr[ExposureSelection]) => {
+  $scope.exposureChart = (anExposure: js.UndefOr[ExposureSelection]) => {
     for {
-      contestID <- aContestID
-      userID <- aUserID
+      contestID <- $routeParams.contestID
+      userID <- gameState.userID
       exposure <- anExposure
       value <- exposure.value
     } {
       portfolioService.getChartData(contestID, userID, value) onComplete {
-        case Success(response) =>
-          updateChart(response.data)
-          $scope.$apply(() => $scope.data = response.data)
+        case Success(response) => updateChartDiv(response.data)
         case Failure(e) =>
           toaster.error(s"Error loading ${exposure.label.orNull}")
           console.error(s"Failed to load exposure data for ${exposure.label.orNull}: ${e.displayMessage}")
@@ -93,7 +91,7 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
     }
   }
 
-  def updateChart(datums: js.Array[ExposureData]): Unit = {
+  private def updateChartDiv(datums: js.Array[ExposureData]): Unit = {
     AmCharts.makeChart(container = "chart_div", AmPieChart(
       dataProvider = datums,
       titleField = "name",
@@ -102,8 +100,8 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
       angle = 30.0,
       outlineAlpha = 0.4,
       depth3D = 15.0,
-     // balloonText = """[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>""",
-     // startDuration = js.undefined,
+      // balloonText = """[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>""",
+      // startDuration = js.undefined,
       export = new Export(enabled = false),
       theme = "light",
       listeners = js.Array(AmPieChart.onClickSlice { clickEvent =>
@@ -112,12 +110,6 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
     ))
     ()
   }
-
-  ///////////////////////////////////////////////////////////////////////////
-  //          Events
-  ///////////////////////////////////////////////////////////////////////////
-
-  $scope.onUserProfileUpdated { (_, profile) => init() }
 
 }
 
@@ -128,26 +120,26 @@ class ExposureController($scope: ExposureControllerScope, $routeParams: Dashboar
 object ExposureController {
 
   /**
+   * Exposure Controller Scope
+   * @author Lawrence Daniels <lawrence.daniels@gmail.com>
+   */
+  @js.native
+  trait ExposureControllerScope extends RootScope {
+    // variables
+    var exposures: js.Array[ExposureSelection] = js.native
+    var options: ChartOptions = js.native
+    var selectedExposure: js.UndefOr[ExposureSelection] = js.native
+
+    // functions
+    var initChart: js.Function0[Unit] = js.native
+    var exposureChart: js.Function1[js.UndefOr[ExposureSelection], Unit] = js.native
+
+  }
+
+  /**
    * Exposure Selection Model
    * @author Lawrence Daniels <lawrence.daniels@gmail.com>
    */
   class ExposureSelection(val label: js.UndefOr[String], val value: js.UndefOr[String]) extends js.Object
-
-}
-
-/**
- * Exposure Controller Scope
- * @author Lawrence Daniels <lawrence.daniels@gmail.com>
- */
-@js.native
-trait ExposureControllerScope extends RootScope {
-  // variables
-  var data: js.Array[_ <: js.Any] = js.native
-  var exposures: js.Array[ExposureSelection] = js.native
-  var options: ChartOptions = js.native
-  var selectedExposure: js.UndefOr[ExposureSelection] = js.native
-
-  // functions
-  var exposureChart: js.Function3[js.UndefOr[String], js.UndefOr[String], js.UndefOr[ExposureSelection], Unit] = js.native
 
 }
