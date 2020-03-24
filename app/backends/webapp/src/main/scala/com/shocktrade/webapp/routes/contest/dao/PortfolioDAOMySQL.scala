9da27d5078ad5daeab230cb09e84b18856ef2ecd
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2020-03-23 20:55:44 -0700

    fixed computation error in portfolio balance

--- app/backends/webapp/src/main/scala/com/shocktrade/webapp/routes/contest/dao/PortfolioDAOMySQL.scala
+++ app/backends/webapp/src/main/scala/com/shocktrade/webapp/routes/contest/dao/PortfolioDAOMySQL.scala
@@ -60,8 +60,7 @@ class PortfolioDAOMySQL(options: MySQLConnectionOptions) extends MySQLDAO(option
   override def findPortfolioBalance(contestID: String, userID: String)(implicit ec: ExecutionContext): Future[Option[PortfolioBalance]] = {
     conn.queryFuture[PortfolioBalance](
       """|SELECT
-         |	  C.contestID, C.name, U.userID, U.username, U.wallet,
-         |	  SUM(P.funds) funds, SUM(SP.lastTrade * PS.quantity) equity,
+         |	C.contestID, C.name, U.userID, U.username, U.wallet, P.funds, SUM(SP.lastTrade * PS.quantity) equity,
          |    SUM(CASE WHEN O.orderType = 'BUY' AND O.fulfilled = 0 THEN SO.lastTrade * O.quantity ELSE 0 END) AS totalBuyOrders,
          |    SUM(CASE WHEN O.orderType = 'SELL' AND O.fulfilled = 0 THEN SO.lastTrade * O.quantity ELSE 0 END) AS totalSellOrders
          |FROM users U
@@ -72,7 +71,7 @@ class PortfolioDAOMySQL(options: MySQLConnectionOptions) extends MySQLDAO(option
          |LEFT JOIN positions PS ON PS.portfolioID = P.portfolioID
          |LEFT JOIN stocks SP ON SP.symbol = PS.symbol
          |WHERE C.contestID = ? AND U.userID = ?
-         |GROUP BY C.contestID, C.name, U.userID, U.username, U.wallet
+         |GROUP BY C.contestID, C.name, U.userID, U.username, U.wallet, P.funds
          |""".stripMargin,
       js.Array(contestID, userID)) map { case (rows, _) => rows.headOption }
   }
