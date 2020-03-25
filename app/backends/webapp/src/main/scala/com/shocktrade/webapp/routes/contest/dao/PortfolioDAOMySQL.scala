fa2dcff93b8d1f605d63c6dd1ae915f8cd68d330
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2020-03-25 13:44:52 -0700

    added default as-of date for portfolio summary

--- app/backends/webapp/src/main/scala/com/shocktrade/webapp/routes/contest/dao/PortfolioDAOMySQL.scala
+++ app/backends/webapp/src/main/scala/com/shocktrade/webapp/routes/contest/dao/PortfolioDAOMySQL.scala
@@ -60,7 +60,7 @@ class PortfolioDAOMySQL(options: MySQLConnectionOptions) extends MySQLDAO(option
   override def findPortfolioBalance(contestID: String, userID: String)(implicit ec: ExecutionContext): Future[Option[PortfolioBalance]] = {
     conn.queryFuture[PortfolioBalance](
       """|SELECT
-         |  C.contestID, C.name, U.userID, U.username, U.wallet, P.funds, MAX(SP.tradeDateTime) AS asOfDate, SUM(SP.lastTrade * PS.quantity) equity,
+         |  C.contestID, C.name, U.userID, U.username, U.wallet, P.funds, IFNULL(MAX(SP.tradeDateTime), NOW()) AS asOfDate, SUM(SP.lastTrade * PS.quantity) equity,
          |  SUM(CASE WHEN O.orderType = 'BUY' AND O.fulfilled = 0 THEN SO.lastTrade * O.quantity ELSE 0 END) AS totalBuyOrders,
          |  SUM(CASE WHEN O.orderType = 'SELL' AND O.fulfilled = 0 THEN SO.lastTrade * O.quantity ELSE 0 END) AS totalSellOrders
          |FROM users U
